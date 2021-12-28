8abf2744aa991c75e17721e7072793d1da723dbb
Ruoshan Huang <ruoshan.huang@gmail.com>
2021-12-28 21:59:20 +0800

    Add github action

--- build.sh
+++ build.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-set -e
+set -xe
 
-GOOS=linux go build ./cmd/apf-agent
+GOOS=linux go build --ldflags "-s" ./cmd/apf-agent
 if command -v upx; then
     upx apf-agent
 fi
 tar -c --uid 0 --uname root --gid 0 --gname root -f agent.tar apf-agent
 mv agent.tar bootstrap
+rm -f apf-agent
 
 go build ./cmd/apf
 
