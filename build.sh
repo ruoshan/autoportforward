797515e44ce81b2c2ec1fb2c2595bdd550a766a5
Ruoshan Huang <ruoshan.huang@gmail.com>
2021-12-28 22:11:26 +0800

    Fix gnu tar options

--- build.sh
+++ build.sh
@@ -6,7 +6,11 @@ GOOS=linux go build --ldflags "-s" ./cmd/apf-agent
 if command -v upx; then
     upx apf-agent
 fi
-tar -c --uid 0 --uname root --gid 0 --gname root -f agent.tar apf-agent
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    tar -c --uid 0 --uname root --gid 0 --gname root -f agent.tar apf-agent
+else
+    tar -c --group=root:0 --owner=root:0 -f agent.tar apf-agent
+fi
 mv agent.tar bootstrap
 rm -f apf-agent
 
