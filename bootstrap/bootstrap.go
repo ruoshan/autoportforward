2d03663cbd9f92f492c40c3bfa45b58ea596a977
Ruoshan Huang <ruoshan.huang@gmail.com>
2022-01-04 18:04:43 +0800

    Add podman support

--- bootstrap/bootstrap.go
+++ bootstrap/bootstrap.go
@@ -11,25 +11,47 @@ import (
 //go:embed agent.tar
 var executables embed.FS
 
+type RTType uint8
+
+const (
+	DOCKER RTType = iota
+	KUBERNETES
+	PODMAN
+)
+
 func bootstrapDocker(containerId string) ([]byte, error) {
 	cmd := exec.Command("docker", "cp", "-", fmt.Sprintf("%s:/", containerId))
 	cmd.Stdin, _ = executables.Open("agent.tar")
 	return cmd.CombinedOutput()
 }
 
+// NB: Due to the limitation of the `kubectl cp/exec`, the target container image must have
+// `tar` in it.
 func bootstrapKubernetes(ns, pod string) ([]byte, error) {
 	cmd := exec.Command("kubectl", "exec", "-i", "-n", ns, pod, "--", "tar", "xf", "-", "-C", "/")
 	cmd.Stdin, _ = executables.Open("agent.tar")
 	return cmd.CombinedOutput()
 }
 
-func Bootstrap(k8s bool, id string) ([]byte, error) {
-	if k8s {
+func bootstrapPodman(containerId string) ([]byte, error) {
+	cmd := exec.Command("podman", "cp", "-", fmt.Sprintf("%s:/", containerId))
+	cmd.Stdin, _ = executables.Open("agent.tar")
+	return cmd.CombinedOutput()
+}
+
+func Bootstrap(rt RTType, id string) ([]byte, error) {
+	switch rt {
+	case DOCKER:
+		return bootstrapDocker(id)
+	case KUBERNETES:
 		splits := strings.SplitN(id, "/", 2)
 		if len(splits) != 2 {
 			return nil, errors.New("invalid kubernetes pod id format ({namespace}/{pod_name}")
 		}
 		return bootstrapKubernetes(splits[0], splits[1])
+	case PODMAN:
+		return bootstrapPodman(id)
+	default:
+		panic("Unknown runtime type")
 	}
-	return bootstrapDocker(id)
 }
